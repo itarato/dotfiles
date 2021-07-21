d7555322338ed03f7dd9cd3c8a4723c2006a8830
Peter Arato <peter.arato@shopify.com>
2021-07-21 17:01:06 -0400

    Update.

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 ln -sf ~/dotfiles/vimrc ~/.vimrc
-# ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/screenrc ~/.screenrc
 
-# if ! command -v rg &> /dev/null; then
-#   sudo apt-get install -y zsh htop mc
-# fi
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# source ~/.zshrc
-
+source ~/.zshrc
 source ~/.bashrc
